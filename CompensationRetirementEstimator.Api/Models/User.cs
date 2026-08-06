69a105f436901d0d618394d6e51c8411bb9faa66
procureX <mkmaaz@outlook.com>
2026-08-05 22:14:57 -0500

    renaming file

--- CompensationRetirementEstimator.Api/Models/User.cs
+++ CompensationRetirementEstimator.Api/Models/User.cs
@@ -1,7 +1,6 @@
 namespace CompensationRetirementEstimator.Api.Models;
 
-public class User
-{
+public class User {
     public int Id { get; set; }
     public string Name { get; set; } = "";
     public int Age { get; set; }
