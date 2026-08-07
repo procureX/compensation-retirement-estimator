17b729986efbf26162cb0a5563df7b1e896d974e
procureX <mkmaaz@outlook.com>
2026-08-06 21:03:06 -0500

    changing variable _context to _db to ensure consistancy

--- CompensationRetirementEstimator.Api/Controllers/RetirementProjectionsController.cs
+++ CompensationRetirementEstimator.Api/Controllers/RetirementProjectionsController.cs
@@ -8,28 +8,28 @@ namespace CompensationRetirementEstimator.Api.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class RetirementProjectionsController : ControllerBase {
-    private readonly AppDbContext _context;
+    private readonly AppDbContext _db;
 
-    public RetirementProjectionsController(AppDbContext context) {
-        _context = context;
+    public RetirementProjectionsController(AppDbContext db) {
+        _db = db;
     }
 
     [HttpPost]
     public async Task<ActionResult<RetirementProjection>> Create([FromBody] RetirementProjection projection) {
         // Validate user exists
-        var user = await _context.Users.FindAsync(projection.UserId);
+        var user = await _db.Users.FindAsync(projection.UserId);
         if (user is null)
             return BadRequest($"User with ID {projection.UserId} does not exist.");
 
-        _context.RetirementProjections.Add(projection);
-        await _context.SaveChangesAsync();
+        _db.RetirementProjections.Add(projection);
+        await _db.SaveChangesAsync();
 
         return CreatedAtAction(nameof(Get), new { id = projection.Id }, projection);
     }
 
     [HttpGet("{id}")]
     public async Task<IActionResult> Get(int id) {
-        var projection = await _context.RetirementProjections.FindAsync(id);
+        var projection = await _db.RetirementProjections.FindAsync(id);
 
         if (projection == null)
             return NotFound();
