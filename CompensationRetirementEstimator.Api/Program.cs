641de0f17ce9179e468b01c88f904bd6af270f06
procureX <mkmaaz@outlook.com>
2026-08-11 00:15:58 -0500

    enables PascalCase to camelCase conversion

--- CompensationRetirementEstimator.Api/Program.cs
+++ CompensationRetirementEstimator.Api/Program.cs
@@ -20,6 +20,11 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddControllers();
 
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+});
+
 // Swagger services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
